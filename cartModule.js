f67b6c936898e3c7a1d62d92a21f7b4a19ae3049
adnanahmed504423@gmail.comgit config --global user.email adnanahmed504423@gmail.com <adnanahmed504423@gmail.com>
2025-01-15 22:15:49 +0530

    intial commit

---
+++ cartModule.js
@@ -0,0 +1,12 @@
+let addtoCard=()=>{
+    return {
+        "name": "adnan",
+        "age":29,
+
+    }
+}
+let changeQuantity=()=>{
+    return "change quantity"
+}
+
+module.exports={addtoCard,changeQuantity}
---
+++ index.js
@@ -0,0 +1,11 @@
+const {addtoCard,changeQuantity} = require("./cartModule");
+
+console.log("welcome to js");
+console.log(20+20);
+let l = [10,20,30,40,60];
+l.forEach((value,index)=>{
+    console.log(value,index)
+})
+console.log(addtoCard()),
+console.log(changeQuantity())
+console.log("adnanahmed")
